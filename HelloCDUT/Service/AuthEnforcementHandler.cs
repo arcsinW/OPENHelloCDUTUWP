0df224c1381fa6ecb67fd7ee4c1450d3ad285a06
arcsinW <wangx86@live.com>
2017-01-01 11:29:53 +0800

    fixed untracked files

---
+++ HelloCDUT/Service/AuthEnforcementHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCDUT.Service
+{
+    public class AuthEnforcementHandler : IAuthEnforcementHandler
+    {
+        public Task CheckUserAuthentication()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
